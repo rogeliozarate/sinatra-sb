692400f01b2aa48c4936a34124312f0a9c2a25d0
Rogelio Zarate <i.am@rogeliozarate.com>
2013-02-07 20:11:36 -0600

    - app styles now served with sass #FTW!

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 require 'haml'
+require 'sass'
 
 
 get '/' do
@@ -10,4 +11,10 @@ end
 
 get '/grid' do
   haml  :grid
+end
+
+get '/stylesheets/*' do
+  content_type 'text/css', :charset => 'utf-8'
+  content_type 'text/css'
+  sass '../stylesheets/'.concat(params[:splat].join.chomp('.css')).to_sym
 end
